a5446874765ca9aaa2de7293e2ff55c4d09f643c
philkazantsev <philkazantsev@gmail.com>
2025-01-21 20:37:10 -0500

    made PID for motor and command ptr

--- src/main/cpp/subsystems/Elevator.cpp
+++ src/main/cpp/subsystems/Elevator.cpp
@@ -1,8 +1,22 @@
 #include "subsystems/Elevator.h"
 
 Elevator::Elevator() {
-    m_followerMotor.SetControl(ctre::phoenix6::controls::Follower{ElevatorConstants::leadmotorID, false});
+    followerMotor.SetControl(ctre::phoenix6::controls::Follower{ElevatorConstants::leadmotorID, false});
+    
+    ElevatorConstants::kElevatorConfig.kP = 0.0;
+    ElevatorConstants::kElevatorConfig.kD = 0.0;
+    ElevatorConstants::kElevatorConfig.kI = 0.0;
+
+    leadMotor.GetConfigurator().Apply(ElevatorConstants::kElevatorConfig);
+    auto statusSignal = leadMotor.GetPosition();
+    auto value = statusSignal.GetDataCopy().value() * 2 * std::numbers::pi * 3_cm;
 };
+
+void Elevator::SetMotorPosition() {
+    ctre::phoenix6::controls::PositionVoltage m_request = ctre::phoenix6::controls::PositionVoltage{0_tr}.WithSlot(0);
+    leadMotor.SetControl(m_request.WithPosition(10_tr));
+
+}
 
 void Elevator::MotorMoveUp() {
     m_leadMotor.SetVoltage(12_V);
