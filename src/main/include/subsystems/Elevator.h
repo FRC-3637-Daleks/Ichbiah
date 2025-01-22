54b13fb8c15fad182e2fefa3526882d21866a2b4
philkazantsev <philkazantsev@gmail.com>
2025-01-21 20:37:10 -0500

    made PID for motor and command ptr

--- src/main/include/subsystems/Elevator.h
+++ src/main/include/subsystems/Elevator.h
@@ -1,13 +1,17 @@
 #pragma once
+
 #include <frc/DigitalInput.h>
 #include <frc/Solenoid.h>
 
-
 #include <ctre/phoenix6/CANcoder.hpp>
 #include <ctre/phoenix6/TalonFX.hpp>
+#include <ctre/phoenix6/configs/Configs.hpp>
+#include <ctre/phoenix6/configs/Configurator.hpp>
 
 
 namespace ElevatorConstants {
+    ctre::phoenix6::configs::Slot0Configs kElevatorConfig{};
+
     int leadmotorID = 50;
     int followermotorID = 70;
     int breakBeamID = 60;
@@ -21,6 +25,7 @@ class Elevator {
     void MotorMoveUp();
     void MotorMoveDown();
     void MotorStop();
+    void SetMotorPosition();
 
     private:
 
