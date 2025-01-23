bdda7070e9f7be735b46b30c6af599fe7ba9e9fe
philkazantsev <philkazantsev@gmail.com>
2025-01-22 20:43:57 -0500

    formatting and elevator/endeffector set pos functions

--- src/main/cpp/subsystems/Elevator.cpp
+++ src/main/cpp/subsystems/Elevator.cpp
@@ -1,22 +1,38 @@
 #include "subsystems/Elevator.h"
+namespace ElevatorConstants {
+    int kLeadmotorID = 50;
+    int kFollowermotorID = 70;
+    int kBreakBeamID = 60;
 
-Elevator::Elevator() {
+    double kP = 0.0;
+    double kI = 0.0;
+    double kD = 0.0;
+}
+
+Elevator::Elevator() : m_leadMotor{ElevatorConstants::kLeadmotorID},
+                       m_followerMotor{ElevatorConstants::kFollowermotorID} {
     //Sets the follower motor
-    m_followerMotor.SetControl(ctre::phoenix6::controls::Follower{ElevatorConstants::kLeadmotorID, false});
-    
-    //Sets and defines the Elevator motor PID config
-    ElevatorConstants::kElevatorConfig.kP = ElevatorConstants::kP;
-    ElevatorConstants::kElevatorConfig.kD = ElevatorConstants::kI;
-    ElevatorConstants::kElevatorConfig.kI = ElevatorConstants::kD;
-    m_leadMotor.GetConfigurator().Apply(ElevatorConstants::kElevatorConfig);
+    using namespace ctre::phoenix6;
 
-    //auto statusSignal = m_leadMotor.GetPosition();
-    //auto value = statusSignal.GetDataCopy().value() * 2 * PI * 3_cm;
+    //Sets and defines the Elevator motor PID config
+    configs::TalonFXConfiguration m_ElevatorConfig;
+    m_followerMotor.SetControl(controls::Follower{ElevatorConstants::kLeadmotorID, false});
+    m_ElevatorConfig.WithSlot0(configs::Slot0Configs{}
+                    .WithKP(ElevatorConstants::kP)
+                    .WithKI(ElevatorConstants::kI)
+                    .WithKD(ElevatorConstants::kD));
+    m_leadMotor.GetConfigurator().Apply(m_ElevatorConfig);    
 };
 
-void Elevator::SetMotorPosition() {
+units::length::centimeter_t Elevator::GetEncoderPosition() {
+    auto statusSignal = m_leadMotor.GetPosition();
+    auto value = statusSignal.GetDataCopy().value() * 2 * std::numbers::pi * 3_cm;
+    return value;
+}
+
+void Elevator::SetMotorPosition(units::angle::turn_t turns) {
     ctre::phoenix6::controls::PositionVoltage m_request = ctre::phoenix6::controls::PositionVoltage{0_tr}.WithSlot(0);
-    m_leadMotor.SetControl(m_request.WithPosition(10_tr));
+    m_leadMotor.SetControl(m_request.WithPosition(turns));
 }
 
 void Elevator::MotorMoveUp() {
@@ -31,4 +47,12 @@ void Elevator::MotorStop() {
     m_leadMotor.SetVoltage(0_V);
 };
 
+frc2::CommandPtr Elevator::WhileUp(){
+    return frc2::cmd::RunEnd ([this]{MotorMoveUp(); },
+                              [this] {MotorStop(); });
+}
 
+frc2::CommandPtr Elevator::WhileDown(){
+    return frc2::cmd::RunEnd ([this]{MotorMoveDown(); },
+                              [this] {MotorStop(); });
+}
