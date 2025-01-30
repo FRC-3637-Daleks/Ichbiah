9adf3158a7747e28da8e30902c3e2d8f41a8fc39
ShrijanS4 <shrijan01.s@gmail.com>
2025-01-29 20:44:59 -0500

    Advanced EndEffector Commands

--- src/main/include/subsystems/EndEffector.h
+++ src/main/include/subsystems/EndEffector.h
@@ -10,6 +10,8 @@
 namespace EndEffectorConstants {
     int kMotorID = 50;
     int kBreakBeamID = 60;
+    int kForwardBreakBeamID = 70;
+    int kBackwardBreakBeamID = 80;
 }
 
 class EndEffector {
@@ -22,12 +24,19 @@ class EndEffector {
     void MotorStop();
 
     bool getBreakBeamState();
+    bool isForwardBreakBeamBroken();
+    bool isBackwardBreakBeamBroken();
 
-    frc2::CommandPtr WhileX();
-    frc2::CommandPtr WhileNotX();
-
+    frc2::CommandPtr WhileOut();
+    frc2::CommandPtr WhileIn();
+    frc2::CommandPtr EffectorIn();
+    frc2::CommandPtr EffectorContinue();
+    frc2::CommandPtr EffectorOut();
+    
     private:
     frc::DigitalInput m_breakbeam{EndEffectorConstants::kBreakBeamID};
+    frc::DigitalInput m_ForwardBreakBeam{EndEffectorConstants::kForwardBreakBeamID};
+    frc::DigitalInput m_BackwardBreakBeam{EndEffectorConstants::kBackwardBreakBeamID};
 
     ctre::phoenix6::hardware::TalonFX m_endEffectorMotor{EndEffectorConstants::kMotorID};
 };
