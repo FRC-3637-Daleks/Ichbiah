b46264d67fc6c5accc8d6a99da838a59a3a94b4b
ShrijanS4 <shrijan01.s@gmail.com>
2025-01-29 20:44:59 -0500

    Advanced EndEffector Commands

--- src/main/cpp/subsystems/EndEffector.cpp
+++ src/main/cpp/subsystems/EndEffector.cpp
@@ -20,7 +20,35 @@ bool EndEffector::getBreakBeamState() {
     return m_breakbeam.Get();
 };
 
-frc2::CommandPtr EndEffector::WhileX(){
+frc2::CommandPtr EndEffector::WhileIn(){
     return frc2::cmd::RunEnd ([this]{ EndEffector::MotorForward(); },
                               [this] {EndEffector::MotorStop(); });
+}
+
+bool EndEffector::isForwardBreakBeamBroken(){
+    return !(m_ForwardBreakBeam.Get());
+}
+bool EndEffector::isBackwardBreakBeamBroken(){
+    return !(m_BackwardBreakBeam.Get());
+}
+
+frc2::CommandPtr EndEffector::EffectorIn() {
+    return frc2::cmd::Run([this] { WhileIn(); })
+    .Until([this]() -> bool {
+        return isForwardBreakBeamBroken();
+    });
+}
+
+frc2::CommandPtr EndEffector::EffectorContinue() {
+    return frc2::cmd::Run([this] { WhileIn(); })
+    .Until([this]() -> bool {
+        return !isBackwardBreakBeamBroken();
+    });
+}
+
+frc2::CommandPtr EndEffector::EffectorOut() {
+    return frc2::cmd::Run([this] { WhileOut(); })
+    .Until([this]() -> bool {
+        return !isForwardBreakBeamBroken();
+    });
 }
