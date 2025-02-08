4d21885b0ccbdfe93ee128780be744a2bb445123
philkazantsev <philkazantsev@gmail.com>
2025-02-08 14:20:58 -0500

    failing to build

--- src/main/cpp/subsystems/EndEffector.cpp
+++ src/main/cpp/subsystems/EndEffector.cpp
@@ -1,5 +1,5 @@
-#pragma once
 #include "subsystems/EndEffector.h"
+#pragma once
 
 namespace EndEffectorConstants {
     int kMotorID = 50;
@@ -8,33 +8,28 @@ namespace EndEffectorConstants {
     int kBackwardBreakBeamID = 80;
 }
 
-EndEffector::EndEffector():
-                        m_ForwardBreakBeam{EndEffectorConstants::kForwardBreakBeamID},
-                        m_BackwardBreakBeam{EndEffectorConstants::kBackwardBreakBeamID},                  
-                        m_endEffectorMotor{EndEffectorConstants::kMotorID}
+EndEffector::EndEffector() :
+    m_BackwardBreakBeam(EndEffectorConstants::kBackwardBreakBeamID),
+    m_ForwardBreakBeam{EndEffectorConstants::kForwardBreakBeamID},
+    m_EndEffectorMotor{EndEffectorConstants::kMotorID}
 {
-
 };
 
 void EndEffector::MotorForward() {
-    m_endEffectorMotor.SetVoltage(12_V);
+    m_EndEffectorMotor.SetVoltage(12_V);
 };
 
 void EndEffector::MotorBack() {
-    m_endEffectorMotor.SetVoltage(-12_V);
+    m_EndEffectorMotor.SetVoltage(-12_V);
 };
 
 void EndEffector::MotorStop() {
-    m_endEffectorMotor.SetVoltage(0_V);
+    m_EndEffectorMotor.SetVoltage(0_V);
 };
 
-frc2::CommandPtr EndEffector::WhileIn(){
-    return frc2::cmd::RunEnd ([this]{ EndEffector::MotorForward(); },
-                              [this] {EndEffector::MotorStop(); });
-}
 
-frc2::CommandPtr EndEffector::WhileOut(){
-    return frc2::cmd::RunEnd ([this]{ EndEffector::MotorBack(); },
+frc2::CommandPtr EndEffector::WhileIn(){
+    return RunEnd([this]{ EndEffector::MotorForward(); },
                               [this] {EndEffector::MotorStop(); });
 }
 
@@ -46,22 +41,29 @@ bool EndEffector::isBackwardBreakBeamBroken(){
 }
 
 frc2::CommandPtr EndEffector::EffectorIn() {
-    return frc2::cmd::Run([this] { WhileIn(); })
+    return Run([this] { WhileIn(); })
     .Until([this]() -> bool {
         return isForwardBreakBeamBroken();
     });
 }
 
 frc2::CommandPtr EndEffector::EffectorContinue() {
-    return frc2::cmd::Run([this] { WhileIn(); })
+    return Run([this] { WhileIn(); })
     .Until([this]() -> bool {
         return !isBackwardBreakBeamBroken();
     });
 }
 
 frc2::CommandPtr EndEffector::EffectorOut() {
-    return frc2::cmd::Run([this] { WhileOut(); })
+    return Run([this] { WhileOut(); })
     .Until([this]() -> bool {
         return !isForwardBreakBeamBroken();
     });
 }
+
+frc2::CommandPtr EndEffector::Intake(){
+    return Run([this]{WhileIn();})
+    .Until([this]() -> bool {
+        return isForwardBreakBeamBroken();
+    });
+} 
