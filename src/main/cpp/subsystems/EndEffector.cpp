95a28b8cbb362b0ca9465fa8d924a6b42e4ab648
Barrel <visvam64@gmail.com>
2025-02-09 18:58:07 -0500

    Fixed build errors. Please take a look at comments.

--- src/main/cpp/subsystems/EndEffector.cpp
+++ src/main/cpp/subsystems/EndEffector.cpp
@@ -1,5 +1,4 @@
 #include "subsystems/EndEffector.h"
-#pragma once
 
 namespace EndEffectorConstants {
     int kMotorID = 50;
@@ -9,28 +8,51 @@ namespace EndEffectorConstants {
 }
 
 EndEffector::EndEffector() :
-    m_BackwardBreakBeam(EndEffectorConstants::kBackwardBreakBeamID),
     m_ForwardBreakBeam{EndEffectorConstants::kForwardBreakBeamID},
+    m_BackwardBreakBeam(EndEffectorConstants::kBackwardBreakBeamID),
     m_EndEffectorMotor{EndEffectorConstants::kMotorID}
-{
-};
+{}
+
+/**
+ * Note from Visvam:
+ * 
+ * Don't for get to define functions you declare in your header file!
+ * 
+ * You also don't need to add semicolons after function definitions.
+ */
+EndEffector::~EndEffector() {}
 
 void EndEffector::MotorForward() {
     m_EndEffectorMotor.SetVoltage(12_V);
-};
+}
 
 void EndEffector::MotorBack() {
     m_EndEffectorMotor.SetVoltage(-12_V);
-};
+}
 
 void EndEffector::MotorStop() {
     m_EndEffectorMotor.SetVoltage(0_V);
-};
+}
 
 
 frc2::CommandPtr EndEffector::WhileIn(){
     return RunEnd([this]{ EndEffector::MotorForward(); },
                               [this] {EndEffector::MotorStop(); });
+}
+
+/**
+ * Note from Visvam:
+ * 
+ * This function was being called without a definition. I wrote this code based on how you wrote WhileIn().
+ * 
+ * Please change it to work how you see fit.
+ */
+
+frc2::CommandPtr EndEffector::WhileOut() {
+    return RunEnd(
+        [this] () { MotorBack(); },
+        [this] () { MotorStop(); }
+    );
 }
 
 bool EndEffector::isForwardBreakBeamBroken(){
