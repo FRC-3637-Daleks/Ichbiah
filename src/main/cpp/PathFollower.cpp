e6d7dc5e5e628d51d60380230c633882b987b3df
Jesuschees <dtrapp@hcrhs.org>
2025-02-19 10:23:01 -0500

    Attempted to implement commands into Pathfollower using static command pointers and maps. For loop in execute is temporary, will find better answer later. NOT BUILDING.

--- src/main/cpp/PathFollower.cpp
+++ src/main/cpp/PathFollower.cpp
@@ -24,6 +24,12 @@ void PathFollower::Initialize() {
   m_timer.Reset();
   m_timer.Start();
   m_field->GetObject("Trajectory")->SetPoses(m_trajectory.GetPoses());
+  auto events = m_trajectory.events;
+  for(auto &e : events) {
+    m_eventPoses.emplace(
+      e.event, m_trajectory.SampleAt(e.timestamp, false)
+      ->GetPose());
+    }
 }
 
 void PathFollower::Execute() {
@@ -32,6 +38,12 @@ void PathFollower::Execute() {
     if (auto desiredState = m_trajectory.SampleAt(currentTime, /* mirror */ false)) {
       auto desiredPose = desiredState->GetPose();
       auto feedForward = desiredState->GetChassisSpeeds();
+      for(auto &e : m_eventPoses) {
+        if(m_driveSubsystem.AtPose(e.second)){
+          auto cmd = getCommand(e.first);
+          cmd.Schedule();
+        }
+      } //error yo
       m_driveSubsystem.DriveToPose(desiredPose, feedForward, {0.0_m, 0.0_m, 0_deg});
     }
 }
@@ -46,6 +58,12 @@ bool PathFollower::IsFinished() {
   return finalPose.has_value() && 
         m_driveSubsystem.AtPose(finalPose.value()) && 
         m_driveSubsystem.IsStopped();
+}
+
+ frc2::CommandPtr PathFollower::getCommand(std::string name) {
+  frc2::Command* k = GetNamedCommands().find(name)->second.get();
+  //CommandPtr(std::unique_ptr<frc::Command>&& command);
+    return frc2::CommandPtr(std::make_unique<frc2::Command>(*k));
 }
 
 frc2::CommandPtr Drivetrain::FollowPathCommand(
