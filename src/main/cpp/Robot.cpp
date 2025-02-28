7c187c7d139eedb67becfed33e74e51ae0594b8b
Jesuschees <dtrapp@hcrhs.org>
2025-02-28 07:41:49 -0500

    Removed auton stuff from auton init and bound it to a trigger instead, (down on pilot dpad). Modified paths to put correct side aligned with coral station. Commented out registering commands, had an issue where we would lose robot comms when running the auton (regardless of whether it was bound by trigger or called via auton routine), unclear if this is due to code or due to bad radios. Cannot replicate this issue in sim.

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -91,12 +91,7 @@ void Robot::DisabledPeriodic() {}
  * This autonomous runs the autonomous command selected by your {@link
  * RobotContainer} class.
  */
-void Robot::AutonomousInit() {
-  m_autonomousCommand = m_container.GetAutonomousCommand();
- if(m_autonomousCommand.has_value()) {
-  m_autonomousCommand->Schedule();
- }
-}
+void Robot::AutonomousInit() {}
 
 void Robot::AutonomousPeriodic() {
 }
