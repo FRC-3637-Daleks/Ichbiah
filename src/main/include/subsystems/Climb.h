c4043387ba16194667d12c10e6a640ec134a83be
Barrel <visvam64@gmail.com>
2025-02-28 21:38:10 -0500

    Added Time-Based Climb Triggers

--- src/main/include/subsystems/Climb.h
+++ src/main/include/subsystems/Climb.h
@@ -1,18 +1,18 @@
 #pragma once
 
 #include "Piston.h"
+
 #include <frc2/command/CommandPtr.h>
-#include <frc2/command/Commands.h>
 #include <frc2/command/SubsystemBase.h>
 
 class Climb : public frc2::SubsystemBase {
-    public:
-        Climb();
-        frc2::CommandPtr ExtendClimb();
-        frc2::CommandPtr RetractClimb();
+public:
+  Climb();
+  frc2::CommandPtr ExtendClimb();
+  frc2::CommandPtr RetractClimb();
 
-        void Periodic() override;
+  void Periodic() override;
 
-    private:
-        Piston m_dualPistons;
+private:
+  Piston m_dualPistons;
 };
--- src/main/include/subsystems/OperatorInterface.h
+++ src/main/include/subsystems/OperatorInterface.h
@@ -9,48 +9,63 @@
 #include <frc2/command/button/JoystickButton.h>
 #include <frc2/command/button/Trigger.h>
 
+#include <frc/DriverStation.h>
+#include <frc/MathUtil.h>
 #include <frc/smartdashboard/SendableChooser.h>
 #include <frc/smartdashboard/SmartDashboard.h>
-#include <frc/MathUtil.h>
 
-
-#include <units/math.h>
 #include <units/acceleration.h>
 #include <units/angle.h>
 #include <units/angular_acceleration.h>
 #include <units/angular_velocity.h>
 #include <units/length.h>
+#include <units/math.h>
 #include <units/moment_of_inertia.h>
 #include <units/velocity.h>
 #include <units/voltage.h>
 
 #include <numbers>
 
+namespace OperatorConstants {
+constexpr auto kClimbExtendTime = 2_min;
+constexpr auto kClimbRetractTime = 2_min + 29_s;
+} // namespace OperatorConstants
+
 class OperatorInterface {
-    
-public: 
-    OperatorInterface();
-    double throttle();
-    units::meters_per_second_t strafe();
-    units::meters_per_second_t fwd();
-    units::revolutions_per_minute_t rot();
-    frc2::Trigger ElevatorL1Trigger = m_copilotController.RightBumper();
-    frc2::Trigger ElevatorL2Trigger = m_copilotController.B();
-    frc2::Trigger ElevatorL3Trigger = m_copilotController.X();
-    frc2::Trigger ElevatorL4Trigger = m_copilotController.Y();
-    frc2::Trigger ElevatorUpTrigger{[this] {return m_copilotController.GetRightY() > 0.5;}};
-    frc2::Trigger ElevatorDownTrigger{[this] {return m_copilotController.GetRightY() < -0.5;}};
-    frc2::Trigger EndEffectorInTrigger = m_copilotController.RightTrigger();
-    frc2::Trigger EndEffectorOutTrigger = m_copilotController.LeftTrigger();
-    frc2::Trigger ClimbUpTrigger = m_copilotController.POVUp();
-    frc2::Trigger ClimbDownTrigger = m_copilotController.POVDown();
-    frc2::Trigger ElevatorIntakeTrigger = m_copilotController.A();
-    frc2::Trigger FollowPathTrigger = m_swerveController.POVDown();
-    frc2::Trigger DriveToPoseTrigger = m_swerveController.POVDown();
-    frc2::Trigger zeroHeadingTrigger = m_swerveController.Button(12);
+
+public:
+  OperatorInterface();
+  double throttle();
+  units::meters_per_second_t strafe();
+  units::meters_per_second_t fwd();
+  units::revolutions_per_minute_t rot();
+  frc2::Trigger ElevatorL1Trigger = m_copilotController.RightBumper();
+  frc2::Trigger ElevatorL2Trigger = m_copilotController.B();
+  frc2::Trigger ElevatorL3Trigger = m_copilotController.X();
+  frc2::Trigger ElevatorL4Trigger = m_copilotController.Y();
+  frc2::Trigger ElevatorUpTrigger{
+      [this] { return m_copilotController.GetRightY() > 0.5; }};
+  frc2::Trigger ElevatorDownTrigger{
+      [this] { return m_copilotController.GetRightY() < -0.5; }};
+  frc2::Trigger EndEffectorInTrigger = m_copilotController.RightTrigger();
+  frc2::Trigger EndEffectorOutTrigger = m_copilotController.LeftTrigger();
+  frc2::Trigger ClimbUpTrigger = m_copilotController.POVUp();
+  frc2::Trigger ClimbDownTrigger = m_copilotController.POVDown();
+  frc2::Trigger ElevatorIntakeTrigger = m_copilotController.A();
+  frc2::Trigger FollowPathTrigger = m_swerveController.POVDown();
+  frc2::Trigger DriveToPoseTrigger = m_swerveController.POVDown();
+  frc2::Trigger zeroHeadingTrigger = m_swerveController.Button(12);
+  frc2::Trigger ClimbExtendTrigger{[this]() -> bool {
+    return frc::DriverStation::GetMatchTime() ==
+           OperatorConstants::kClimbExtendTime;
+  }};
+  frc2::Trigger ClimbRetractTrigger{[this]() -> bool {
+    return frc::DriverStation::GetMatchTime() ==
+           OperatorConstants::kClimbRetractTime;
+  }};
 
 private:
-    frc2::CommandXboxController m_swerveController;
-    frc2::CommandXboxController m_copilotController;
-    bool IsRed();
+  frc2::CommandXboxController m_swerveController;
+  frc2::CommandXboxController m_copilotController;
+  bool IsRed();
 };
