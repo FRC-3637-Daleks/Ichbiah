8ba266fc1f3cf378207ea60fda28f9548613f2d5
ShrijanS4 <shrijan01.s@gmail.com>
2025-03-05 20:46:53 -0500

    Continued LED state progress

--- src/main/cpp/subsystems/LEDSubsystem.cpp
+++ src/main/cpp/subsystems/LEDSubsystem.cpp
@@ -2,71 +2,93 @@
 
 #include "frc/smartdashboard/SmartDashboard.h"
 
-LEDSubsystem::LEDSubsystem()
-{
-    // Setup LED Segments
-    frc::LEDPattern m_black = frc::LEDPattern::Solid(frc::Color::kBlack);
-    m_ledSegments[0] = std::span<frc::AddressableLED::LEDData>{m_ledBuffer.begin(), m_ledSegmentLengths[0]};
+LEDSubsystem::LEDSubsystem() {
+  // Setup LED Segments
+  frc::LEDPattern m_black = frc::LEDPattern::Solid(frc::Color::kBlack);
+  m_ledSegments[0] = std::span<frc::AddressableLED::LEDData>{
+      m_ledBuffer.begin(), m_ledSegmentLengths[0]};
+  m_spanPatterns.push_back(m_black);
+
+  auto currIter{m_ledSegments[0].end()};
+  for (int i = 1; i < kNumSpans; i++) {
+    m_ledSegments[i] = std::span<frc::AddressableLED::LEDData>{
+        currIter, m_ledSegmentLengths[i]};
+    currIter = m_ledSegments[i].end();
     m_spanPatterns.push_back(m_black);
+  }
 
-    auto currIter{m_ledSegments[0].end()};
-    for (int i = 1; i < kNumSpans; i++) {
-        m_ledSegments[i] = std::span<frc::AddressableLED::LEDData>{currIter, m_ledSegmentLengths[i]};
-        currIter = m_ledSegments[i].end();
-        m_spanPatterns.push_back(m_black);
+  // Setup AddressibleLED Object
+  m_led.SetLength(kLength);
+  m_led.SetData(m_ledBuffer);
+  m_led.Start();
+
+  m_black.ApplyTo(m_ledBuffer);
+  m_led.SetData(m_ledBuffer);
+
+  setState(LEDSTATE::Default);
+}
+
+void LEDSubsystem::Periodic() {
+  switch (m_currState) {
+  case LEDSTATE::Default:
+    const bool hasCoral =
+        frc::SmartDashboard::GetBoolean(SDCONST::coralInIntake, false);
+    if (hasCoral)
+      ;
+    {
+      setState(LEDSTATE::CoralInIntake);
+      countDown = 1000;
     }
-
-    // Setup AddressibleLED Object 
-    m_led.SetLength(kLength);
-    m_led.SetData(m_ledBuffer);
-    m_led.Start();
-
-    m_black.ApplyTo(m_ledBuffer);
-    m_led.SetData(m_ledBuffer);
-
-    setState(LEDSTATE::Default);
-}
-
-void LEDSubsystem::Periodic()
-{
-    for (int i = 0; i < kNumSpans; i++){
-        m_spanPatterns[i].ApplyTo(getSegment(i));
+    break;
+  case LEDSTATE::CoralInIntake:
+    if (countDown <= 0.0) {
+      setState(LEDSTATE::DefaultWithCoral);
     }
-    m_led.SetData(m_ledBuffer);
+  }
+
+  for (int i = 0; i < kNumSpans; i++) {
+    m_spanPatterns[i].ApplyTo(getSegment(i));
+  }
+
+  m_led.SetData(m_ledBuffer);
 }
 
-void LEDSubsystem::setState(LEDSTATE state)
-{
-    switch(state){
-        case LEDSTATE::Default: 
-        const bool isRed = frc::SmartDashboard::GetBoolean("isRed", false);
-        if (isRed){
-            setAllSpanPatterns(frc::LEDPattern::Solid(frc::Color::kFirstRed));
-        } else {
-            setAllSpanPatterns(frc::LEDPattern::Solid(frc::Color::kFirstBlue));
-        }
-        break;
+void LEDSubsystem::setState(LEDSTATE state) {
+  switch (state) {
+  case LEDSTATE::CoralInIntake:
+    setAllSpanPatterns(frc::LEDPattern::Solid(frc::Color::kYellow));
+    break;
+  case LEDSTATE::CoralDropped:
+    setAllSpanPatterns(frc::LEDPattern::Solid(frc::Color::kGreen));
+    break;
+  default:
+    const bool isRed = frc::SmartDashboard::GetBoolean(SDCONST::isRed, false);
+    if (isRed) {
+      setAllSpanPatterns(frc::LEDPattern::Solid(frc::Color::kFirstRed));
+    } else {
+      setAllSpanPatterns(frc::LEDPattern::Solid(frc::Color::kFirstBlue));
     }
+    break;
+  }
+
+  m_currState = state;
 }
 
-std::span<frc::AddressableLED::LEDData> LEDSubsystem::getSegment(int segment)
-{
-   return m_ledSegments[segment];
+std::span<frc::AddressableLED::LEDData> LEDSubsystem::getSegment(int segment) {
+  return m_ledSegments[segment];
 }
 
-std::span<frc::AddressableLED::LEDData> LEDSubsystem::getSegment(LEDSEGMENT segment)
-{
-    return getSegment(static_cast<int>(segment));
+std::span<frc::AddressableLED::LEDData>
+LEDSubsystem::getSegment(LEDSEGMENT segment) {
+  return getSegment(static_cast<int>(segment));
 }
 
-frc::LEDPattern &LEDSubsystem::getSpanPattern(LEDSEGMENT segment)
-{
-    return m_spanPatterns[static_cast<int>(segment)];
+frc::LEDPattern &LEDSubsystem::getSpanPattern(LEDSEGMENT segment) {
+  return m_spanPatterns[static_cast<int>(segment)];
 }
 
-void LEDSubsystem::setAllSpanPatterns(const frc::LEDPattern &pattern)
-{
-    for (int i = 0; i < kNumSpans; i++){
-        m_spanPatterns[i] = pattern;
-    }
+void LEDSubsystem::setAllSpanPatterns(const frc::LEDPattern &pattern) {
+  for (int i = 0; i < kNumSpans; i++) {
+    m_spanPatterns[i] = pattern;
+  }
 }
--- src/main/include/subsystems/LEDSubsystem.h
+++ src/main/include/subsystems/LEDSubsystem.h
@@ -1,45 +1,55 @@
 #pragma once
 
-#include <frc2/command/SubsystemBase.h>
 #include <frc/AddressableLED.h>
 #include <frc/LEDPattern.h>
+#include <frc2/command/SubsystemBase.h>
+
+namespace SDCONST {
+const std::string isRed{"isRed"};
+const std::string coralInIntake{"coralInIntake"};
+} // namespace SDCONST
 
 class LEDSubsystem : public frc2::SubsystemBase {
-    public:
-    LEDSubsystem();
-    void Periodic() override;
+public:
+  LEDSubsystem();
+  void Periodic() override;
 
-    private:
-    static constexpr int kPort{0};
-    static constexpr int kNumSpans{3};
-    static constexpr int kLength{150};
-    std::array<frc::AddressableLED::LEDData, kLength> m_ledBuffer{};
-    std::array<uint32_t, kNumSpans> m_ledSegmentLengths{47, 26 ,47};
+private:
+  static constexpr int kPort{0};
+  static constexpr int kNumSpans{3};
+  static constexpr int kLength{150};
+  std::array<frc::AddressableLED::LEDData, kLength> m_ledBuffer{};
+  std::array<uint32_t, kNumSpans> m_ledSegmentLengths{47, 26, 47};
 
   enum class LEDSEGMENT {
-        ElevLeft,
-        ElevTop,
-        ElevRight,
-    };
-  
-    enum class LEDSTATE{
-        Default,
-    };
+    ElevLeft,
+    ElevTop,
+    ElevRight,
+  };
 
-    LEDSTATE m_currState {LEDSTATE::Default};
+  enum class LEDSTATE {
+    Default,
+    CoralInIntake,
+    DefaultWithCoral,
+    CoralDropped,
+  };
 
-    std::vector<frc::LEDPattern > m_spanPatterns{};
+  LEDSTATE m_currState{LEDSTATE::Default};
 
-    //LED Strip has 60 LEDs / meter
-    units::meter_t kLedSpacing{1 / 60.0};
+  std::vector<frc::LEDPattern> m_spanPatterns{};
 
-    frc::AddressableLED m_led{kPort};
+  // LED Strip has 60 LEDs / meter
+  units::meter_t kLedSpacing{1 / 60.0};
 
-    std::array<std::span<frc::AddressableLED::LEDData>,kNumSpans> m_ledSegments;
+  frc::AddressableLED m_led{kPort};
 
-    void setState(LEDSTATE state);
-    std::span<frc::AddressableLED::LEDData> getSegment(int segment);
-    std::span<frc::AddressableLED::LEDData> getSegment(LEDSEGMENT segment);
-    frc::LEDPattern& getSpanPattern(LEDSEGMENT segment);
-    void setAllSpanPatterns(const frc::LEDPattern& pattern);
+  std::array<std::span<frc::AddressableLED::LEDData>, kNumSpans> m_ledSegments;
+
+  double countDown = 0.0;
+
+  void setState(LEDSTATE state);
+  std::span<frc::AddressableLED::LEDData> getSegment(int segment);
+  std::span<frc::AddressableLED::LEDData> getSegment(LEDSEGMENT segment);
+  frc::LEDPattern &getSpanPattern(LEDSEGMENT segment);
+  void setAllSpanPatterns(const frc::LEDPattern &pattern);
 };
